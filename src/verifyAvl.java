8e7cd9769c0e66ce9835cdf562a9024add3dbb6c
Vishnu Kartha <vkartha@uw.edu>
2021-04-28 20:25:07 -0700

    madeVerifyAVL O(n) replaced the getHeight method with verifyHeights

--- src/verifyAvl.java
+++ src/verifyAvl.java
@@ -3,29 +3,27 @@ public class verifyAvl {
     // satisfies the BST Property, the AVL Balance Condition,
     // and contains the correct height information.
     public static boolean verifyAVL(AVLNode root) {
-        // starts recursion with LONG.MIN_VALUE and LONG.MAX_VALUE,
-        // so that the root trivially passes the BST condition.
-        return helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        if(root == null) {
+            return true;
+        } else {
+            // Note: starts recursion with LONG.MIN_VALUE and LONG.MAX_VALUE,
+            // so that the root trivially passes the BST condition.
+            return verifyHeights(root) != null && isValidAVL(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        }
     }
 
     // helper used to determine  whether the tree starting at root
-    // is a valid AVL tree and contains the correct height information.
-    private static boolean helper(AVLNode root, long min, long max) {
+    // is a valid AVL tree.
+    private static boolean isValidAVL(AVLNode root, long min, long max) {
         if(root == null) {
             return true;
         }
-        // 1. Check to make sure the height of the current node is correct
-        if(root.height != getHeight(root)){
-            return false;
-        }
-
-        // 2. checks to see if the node's key satisfies the BST condition
+        // 1. checks to see if the node's key satisfies the BST condition
         if(root.key <= min || root.key >= max) {
             return false;
         }
-
-        // 3. recurse to the left and right subtrees and check if they are balanced.
-        return helper(root.left,min, root.key) && helper(root.right,root.key, max) && isBalanced(root);
+        // 2. recurse to the left and right subtrees and check if they are balanced.
+        return isValidAVL(root.left,min, root.key) && isValidAVL(root.right,root.key, max) && isBalanced(root);
     }
     // checks whether the left and right subtrees of an AVLNode are balanced. NOTE: Assumes that the
     // height information stored in the nodes are valid.
@@ -41,17 +39,29 @@ public class verifyAvl {
             rightTreeHeight = 0;
         } else {
             rightTreeHeight= root.right.height;
-
         }
         return Math.abs(rightTreeHeight - leftTreeHeight) <= 1;
     }
 
-    // Returns the height of the tree  starting at AVLNode root.
-    private static int getHeight(AVLNode root) {
-        if(root == null){
-            return -1;
-        }else{
-            return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+      // returns the height of the root if the height information in the tree starting at root is correct.
+      // returns null if the height information in the tree starting at root is incorrect.
+      private static Integer verifyHeights(AVLNode root){
+          if(root == null){
+              return -1;
+          } else {
+            Integer leftRecurse = verifyHeights(root.left);
+            Integer rightRecurse = verifyHeights(root.right);
+            // check if the height info in L and R subtrees are correct
+            if(leftRecurse == null || rightRecurse == null){
+                return null;
+            }
+            int foundHeight = 1 + Math.max(leftRecurse, rightRecurse);
+            if(foundHeight != root.height){
+                // this node has incorrect height information
+                return null;
+            } else {
+                return foundHeight;
+            }
         }
-    }
+      }
 }
